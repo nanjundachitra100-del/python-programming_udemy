c5e876443f07163182836b95f66a62ed1c53a474
ChitraShree Gowda <nanjundachitra100@gmail.com>
2026-07-20 23:41:44 +0530

    get_post

--- Flask_Framework.py/get_post.py
+++ Flask_Framework.py/get_post.py
@@ -1,21 +1,22 @@
-from flask import Flask , render_template
+from flask import Flask, render_template, request
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "welcome to the page!"
+    return "Welcome to the page!"
 
-@app.route("/index")
-def object():
+@app.route("/index", methods=["GET"])
+def index():
     return render_template("index.html")
 
+@app.route("/form", methods=["GET", "POST"])
+def form():
+    if request.method == "POST":
+        name=request.form['name']
+        return f"Hello {name}"
 
-
-
-
-
+    return render_template("form.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
